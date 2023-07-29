de9085d73c2d52c80b0d9c77f0e2371991be7fe0
pocoyo <112208359+pocoyojs@users.noreply.github.com>
2023-07-28 23:56:23 -0300

    Initial commit

---
+++ assets/config.js
@@ -0,0 +1,21 @@
+const config = {
+    
+    USERNAME: "Hugoto69#4975",
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: "Welcome to my Discord Profile!",
+
+    BADGES: [
+        { id: "HypeSquad Balance Badge", image: "https://discord.com/assets/9fdc63ef8a3cc1617c7586286c34e4f1.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-2", image: "assets/img/BlueTechInc.png", link: "https://discord.gg/bnuC8yzFqN", name: "BlueTechnician Incorporated" },
+        { id: "project-3", image: "assets/img/ISK.png", link: "https://discord.gg/Vj5BHQzSTk", name: "I.S.K. Incorporated" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Hugoto69" },
+    ],
+
+}
